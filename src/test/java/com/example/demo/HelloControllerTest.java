39d3b25a33bee2a51f8afec498b92088436c8048
worldpeacez0991 <worldpeacez0991@gmail.com>
2023-07-11 18:17:49 +0800

    Initial commit

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+//Error2:
+//@SpringBootTest
+
+//Solution2:
+@ComponentScan(basePackages = "com.example.springboot")
+@SpringBootTest(classes = HelloControllerTest.class)
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+	}
+}
